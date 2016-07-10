7be927e45887579b24ba5ba8fdbf021029fcf712
BigMikeBaller <mjm275@pitt.edu>
2016-07-10 14:19:05 -0400

    Added sample group data

--- facespace_data.sql
+++ facespace_data.sql
@@ -1,1 +1,12 @@
 --sample data for facespace
+
+INSERT INTO Groups VALUES(1, 'CS: GO', 'Counter Strike G.O. Players', 6);
+INSERT INTO Groups VALUES(2, 'Overwatch', 'Overwatch Players', 6);
+INSERT INTO Groups VALUES(3, 'LoL', 'League of Legends Players', 5);
+INSERT INTO Groups VALUES(4, 'Rocket League', 'Rocket League Players', 3);
+INSERT INTO Groups VALUES(5, 'SC2', 'Starcraft 2 Players', 4);
+INSERT INTO Groups VALUES(6, 'Battlefield 1', 'Battlefield 1 Players', 10);
+INSERT INTO Groups VALUES(7, 'No Man\'s Sky', 'No Man\'s Sky Players', 5);
+INSERT INTO Groups VALUES(8, 'COD4', 'Call of Duty 4: Modern Warfare Players', 6);
+INSERT INTO Groups VALUES(9, 'BBB', 'Bingle Bongle Boongle Players', 2);
+INSERT INTO Groups VALUES(10, 'Wonderputt', 'Wonderputt Players', 4);
