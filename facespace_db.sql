e5f1bf3bd3244af568be5dcbd76fc61752517224
job96 <job96@pitt.edu>
2016-07-10 18:33:07 -0400

    Did some stuff

--- facespace_db.sql
+++ facespace_db.sql
@@ -52,6 +52,6 @@ CREATE TABLE Messages
 
 CREATE TABLE Recipients
 (
-	messageId number(10) PRIMARY KEY,
+	messageId number(10) PRIMARY KEY FOREIGN KEY REFERENCES Messages(messageId),
 	userId number(10) FOREIGN KEY REFERENCES Profiles(userId)
 );
