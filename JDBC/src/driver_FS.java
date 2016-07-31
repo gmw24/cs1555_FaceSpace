2899c05eec00c96dab355e6269950ba3a8b3d8aa
WootWoot-42 <job96@pitt.edu>
2016-07-31 17:35:30 -0400

    Add files via upload

--- JDBC/src/driver_FS.java
+++ JDBC/src/driver_FS.java
@@ -1,42 +1,52 @@
-/*
- * This is the driver program for FaceSpace. It runs all the functions and displays working functionality.
- * Please note: before running this, execute the 3 SQL files present in the repository to make sure the db 
- * is in a consistent state. Run them in this order:
- * facespace_db.sql
- * facespace_data.sql
- * facespace_integrity.sql
- */
-
-import java.sql.SQLException;
 import java.util.Scanner;
 
 
 public class driver_FS {
 	
-	public static void main(String[] args) throws SQLException {
+		
+	
+	public static void main(String[] args) {
 		
 		Scanner inScan=new Scanner(System.in);
 		FaceSpace theFS = new FaceSpace();
-		
-		System.out.println("First we initiate the connection.");
-		theFS.initConnection();
 		
 		System.out.println("Demostrating our FaceSpace functionality!");
 		System.out.print("Press enter to begin:");
 		inScan.next();
 		
-		System.out.println("Will begin by proving createUser() and searchForUser() work...");
+		System.out.println("Will begin by proving createUser() works with searchForUser()...");
 		System.out.println("Calling searchForUser(\"Jim Thorpe\"");
-		theFS.searchForUser("Thorp");
+		theFS.searchForUser("Jim Thorpe");
 		System.out.print("Press enter to continue:");
 		inScan.next();
 		System.out.println("Calling createUser(\"Jim\",\"Thorpe\",\"jthorpe@gmail.com\",1995,1,1)");
 		theFS.createUser("Jim","Thorpe","jthorpe@gmail.com",1995,1,1);
-		System.out.println("Calling searchForUser(\"Thorp\"");
-		theFS.searchForUser("Thorp");
+		System.out.println("Calling searchForUser(\"Jim Thorpe\"");
+		theFS.searchForUser("Jim Thorpe");
+		System.out.println("Press enter to continue:");
+		inScan.next();
+
 		System.out.print("Done with this part! Press enter to continue:");
 		inScan.next();
 		
+		System.out.println("Will now prove initiateFriendship(), establishFriendship(), and displayFriends() work...");
+		System.out.println("Calling displayFriends(1)");
+		theFS.displayFriends(1);
+		System.out.print("Press enter to continue:");
+		inScan.next();
+		System.out.println("Calling initiateFriendship(1,6)");
+		int fNum = theFS.initiateFriendship(1,6);
+		System.out.println("Calling displayFriends(1)");
+		theFS.displayFriends(1);
+		System.out.print("Press enter to continue:");
+		inScan.next();
+		System.out.println("Calling establishFriendship(1,6)");
+		theFS.establishFriendship(6,fNum);
+		System.out.println("Calling displayFriends(1)");
+		theFS.displayFriends(1);
+		System.out.print("Done with this part! Press enter to continue:");
+		inScan.next();
+
 		System.out.println("Will now prove initiateFriendship(), establishFriendship(), and displayFriends() work...");
 		System.out.println("Calling displayFriends(1)");
 		theFS.displayFriends(1);
@@ -89,5 +99,30 @@ public class driver_FS {
 		System.out.println("Searching for three degrees between userIds 69 and 75");
 		theFS.threeDegrees(69, 75);
 		
+		System.out.println("Will now prove displayMessages() and displayNewMessages() work...");
+		System.out.println("Calling displayMessages(1)");
+		theFS.displayMessages(1);
+		System.out.print("Press enter to continue:");
+		inScan.next();
+		System.out.println("Calling displayNewMessages(1)");
+		theFS.displayNewMessages(1);
+		System.out.print("Done with this part! Press enter to continue:");
+		inScan.next();
+		
+		System.out.println("Proving that createGroup() and addToGroup() work together...");
+		System.out.println("Calling createGroup()...");
+		theFS.createGroup("Goofy Goobers","We're all Goofy Goobers, yeah",6);
+		System.out.println("Adding a user to this group...");
+		theFS.addToGroup("Britton","Jordan","Goofy Goobers");
+		System.out.print("Done with this part! Press enter to continue:");
+		inScan.next();
+
+		System.out.println("Proving that sendMessageToUser() works...");
+		theFS.sendMessagetoUser(1,47,"I h8 U","You sold me a bad hairdo");
+		System.out.println("And sendMessageToGroup()...");
+		theFS.sendMessageToGroup(61,5,"We Win","All your base are belong to us");
+		System.out.print("Done with sending messages. Press enter to continue");
+		inScan.next();
 	}
+
 }
