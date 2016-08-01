d10be6e5da199b25d87bbc8848a903231a669eeb
Gabriel Wells <gmw24@pitt.edu>
2016-08-01 11:07:42 -0400

    Fixed initiate friendship bug, membership limit check

--- JDBC/src/FaceSpace.java
+++ JDBC/src/FaceSpace.java
@@ -148,9 +148,11 @@ public class FaceSpace {
     			String query = "SELECT MAX(friendshipId) AS count FROM Friendships";
     			ResultSet res = stmt.executeQuery(query);
     			
-    			while(rs.next()){
-    				numFriendship = rs.getInt("count");
+    			while(res.next()){
+    				numFriendship = res.getInt("count");
     			}
+    			
+    			//System.out.println(numFriendship);
     			
                 res = stmt.executeQuery("SELECT * FROM Friendships");
         	 	String Query = "INSERT INTO Friendships VALUES (?,?,?,?,?)";
@@ -1398,15 +1400,15 @@ public class FaceSpace {
     		} catch (SQLException e) {
     			System.out.println("Cannot close Statement. Machine error: "+e.toString());
     		}
-//		finally
-//		{
-//		 	try {
-//    			if (statement !=null) statement.close();
-//    		} catch (SQLException e) {
-//    			System.out.println("Cannot close Statement. Machine error: "+e.toString());
-//    		}
-//			dbconn.close();
-//		}
+		finally
+		{
+		 	try {
+    			if (statement !=null) statement.close();
+    		} catch (SQLException e) {
+    			System.out.println("Cannot close Statement. Machine error: "+e.toString());
+    		}
+			dbconn.close();
+		}
 	}
 }
 }
